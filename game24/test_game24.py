8287153dd539d1d32ef83466fa8ad4dc51e7ae23
jitechpc3 <you@example.com>
2021-08-13 16:33:56 +0400

    adding a test for the print_title function

--- game24/test_game24.py
+++ game24/test_game24.py
@@ -4,22 +4,18 @@ import pytest
 from .gameconsole import GameConsole as gm
 
 
-@pytest.mark.parametrize('title, result', [('12 + 12 + 12 - 12\n
-                         12 × (12 + 12) ÷ 12\n12 + 12 × 12 ÷ 12',
-                         '\n12 + 12 + 12 - 12\n12 × (12 + 12) ÷ 12\n
-                         12 + 12 × 12 ÷ 12\n\n'),
-                         ('4 × (1 + 2 + 3)\n1 × 2 × 3 × 4\n
-                          (1 + 3) × (2 + 4)',
-                          '\n4 × (1 + 2 + 3)\n1 × 2 × 3 × 4\n
-                          (1 + 3) × (2 + 4)\n\n')])
+@pytest.mark.parametrize('title, result', 
+                         [('12 + 12 + 12 - 12\n12 × (12 + 12) ÷ 12\n12 + 12 × 12 ÷ 12',
+                         '\n12 + 12 + 12 - 12\n12 × (12 + 12) ÷ 12\n12 + 12 × 12 ÷ 12\n\n'),
+                         ('4 × (1 + 2 + 3)\n1 × 2 × 3 × 4\n(1 + 3) × (2 + 4)',
+                         '\n4 × (1 + 2 + 3)\n1 × 2 × 3 × 4\n(1 + 3) × (2 + 4)\n\n')])
 def test_print_title(capsys, title, result):
     gm.print_title(title)
     out, err = capsys.readouterr()
     assert out == result
 
 
-@pytest.mark.parametrize('title, result', [(None, None), ('4 × (5 + 9 - 8)\n
-                         8 + 4 × (9 - 5)',
+@pytest.mark.parametrize('title, result', [(None, None), ('4 × (5 + 9 - 8)\n8 + 4 × (9 - 5)',
                          '4 × (5 + 9 - 8)\n8 + 4 × (9 - 5)')])
 def test_print_title_negativ(capsys, title, result):
     gm.print_title(title)
