30a536c50cbc2c2b82fb64ff23c0464a32f5dd0e
Grigor <84573365+Adoyan-Grigor@users.noreply.github.com>
2021-08-18 11:32:04 +0400

    Update test_game24.py
    adding tests for functional raw_input_ex and ui_menu

--- game24/test_game24.py
+++ game24/test_game24.py
@@ -21,3 +21,17 @@ def test_print_title_negativ(capsys, title, result):
     gm.print_title(title)
     out, err = capsys.readouterr()
     assert out != result
+
+@pytest.mark.xfail
+@pytest.mark.parametrize('input_output', [('name'), ('HeLlO'), (''), ('12')])
+def test_raw_input_ex(input_output):
+    with mock.patch.object(builtins, 'input', lambda _: input_output):
+       assert gm.raw_input_ex() == input_output
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize('r', [('1'), ('2'), ('3'), ('p'), ('c'), ('q')])
+def test_ui_menu(r):
+    with mock.patch.object(builtins, 'input', lambda _: r):
+       assert gm.ui_menu(menu, choices, eof = True) == r
+
