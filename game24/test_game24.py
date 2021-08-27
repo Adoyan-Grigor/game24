17ccf990c45b5a2416db79de6d78ccc9a6b8966a
Grigor <84573365+Adoyan-Grigor@users.noreply.github.com>
2021-08-27 10:58:09 +0400

    adding a test for the ui_menu_and_expr function

--- game24/test_game24.py
+++ game24/test_game24.py
@@ -57,7 +57,7 @@ def test_ui_check_answer_negative(capsys, r):
 
         
 @pytest.mark.xfail
-@pytest.mark.parametrize('test', [(1), (2), (3), (4), (5)])
+@pytest.mark.parametrize('test', [(1), (2), (3), (4), (5), ('h'), ('s'), ('b'), ('q')])
 def test_ui_menu_and_expr(capsys, test):
     menu = '1. Definitely no solutions (n)\n2. Give me a hint (h)\n3. I gave up, show me the answer (s)\n4. Back to the main menu (b)\n5. Quit the game (q)'
     choices = '1n2h3s4b5q'
@@ -67,25 +67,29 @@ def test_ui_menu_and_expr(capsys, test):
     hand_ui_check = ''
     result = ''
     final_result = ''
-    for i in hand_ints:
-        hand_ui_check += str(i) + ' '
-    with mock.patch.object(builtins, 'input', lambda _: hand_ui_check):
-        gg.ui_check_answer()
-        out, err = capsys.readouterr()
-        if out != '\nSeems no solutions\n\n':
-            for l in out:
-                if l == '×':
-                    result += '*'
-                elif l == '÷':
-                    result += '/'
-                else:
-                    result += l
-            for k in result[1: ]:
-                if k == '\n':
-                    break
-                final_result += k)
-            with mock.patch.object(builtins, 'input', lambda _: final_result):
-                assert gg.ui_menu_and_expr(menu, choices, eof = True) == calc.parse(final_result)
-        else:
-            with mock.patch.object(builtins, 'input', lambda _: 'n'):
-                assert gg.ui_menu_and_expr(menu, choices, eof = True) == 'n'
+    if test in range(1, 6):
+        for i in hand_ints:
+            hand_ui_check += str(i) + ' '
+        with mock.patch.object(builtins, 'input', lambda _: hand_ui_check):
+            gg.ui_check_answer()
+            out, err = capsys.readouterr()
+            if out != '\nSeems no solutions\n\n':
+                for l in out:
+                    if l == '×':
+                        result += '*'
+                    elif l == '÷':
+                        result += '/'
+                    else:
+                        result += l
+                for k in result[1: ]:
+                    if k == '\n':
+                        break
+                    final_result += k
+                with mock.patch.object(builtins, 'input', lambda _: final_result):
+                    assert gg.ui_menu_and_expr(menu, choices, eof = True) == calc.parse(final_result)
+            else:
+                with mock.patch.object(builtins, 'input', lambda _: 'n'):
+                    assert gg.ui_menu_and_expr(menu, choices, eof = True) == 'n'
+    else:
+        with mock.patch.object(builtins, 'input', lambda _: test):
+            assert gg.ui_menu_and_expr(menu, choices, eof = True) == test
