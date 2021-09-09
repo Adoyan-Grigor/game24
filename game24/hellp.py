a03b240ee038cafe06faf5eec099afb1ffec78f7
Grigor <84573365+Adoyan-Grigor@users.noreply.github.com>
2021-09-09 12:27:26 +0400

    Add files via upload

---
+++ game24/hellp.py
@@ -0,0 +1,55 @@
+#!usr/bin/python3
+import pytest
+import mock
+import builtins
+import sys
+
+
+from game24.gameconsole import GameConsole as gc
+from game24.game import Game as gm
+from . import calc
+
+def hellp_ui_check_answer(capsys, r):
+    gg = gc()
+    result = ''
+    final_result = ''
+    with mock.patch.object(builtins, 'input', lambda _: r):
+        gg.ui_check_answer()
+        out, err = capsys.readouterr()
+        if out == '\nSeems no solutions\n\n':
+            final_result = 'n'
+        else:
+            for i in out[1: ]:
+                if i == '\n':
+                    break
+                result += i
+            for i in result:
+                if i == '×':
+                    final_result += '*'
+                elif i == '÷':
+                    final_result += '/'
+                else:
+                    final_result += i
+        return final_result
+
+
+def hellp_new_hand():
+    gg = gc()
+    res_hand = ''
+    hand = gg.new_hand()
+    hand_ints = hand.integers
+    for i in hand_ints:
+        res_hand += str(i) + ' '
+    return res_hand
+
+
+def hellp_new_hand_cards(cards):
+    llist = []
+    for i in range(len(cards[44: 48])):
+        llist.append(cards[i])
+    print(llist)
+    cards = []
+    while len(cards) < 53:
+        for i in llist:
+            cards.append(i)
+    return cards
