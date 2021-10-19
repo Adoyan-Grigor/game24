d6732f4cc95208f86f063c07d235e34397275e36
Grigor <84573365+Adoyan-Grigor@users.noreply.github.com>
2021-10-19 16:26:56 +0400

    adding a function to improve tests

--- game24/hhelp.py
+++ game24/hhelp.py
@@ -7,6 +7,7 @@ import readline
 
 
 from game24.gameconsole import GameConsole as gc
+from game24.game import Hand
 from game24.game import Game as gm
 from . import calc
 
@@ -14,15 +15,7 @@ from . import calc
 class GGameConsole(gc):
     def raw_input_ex(prompt='', default=''):
         '''enhance raw_input to support default input and also flat EOF'''
-        try:
-            readline.set_startup_hook(lambda: readline.insert_text(default))
-            try:
-                return input(prompt)
-            finally:
-                readline.set_startup_hook()
-
-        except EOFError:
-            return INPUT_EOF
+        return input(prompt)
 
 gc = GGameConsole
 
@@ -38,28 +31,16 @@ def hellp_ui_check_answer(capsys, r):
         if out == '\nSeems no solutions\n\n':
             final_result = 'n'
         else:
-            for i in out[1: ]:
+            for i in out:
                 if i == '\n':
                     break
-                result += i
-            for i in result:
-                if i == '×':
+                elif i == '×':
                     final_result += '*'
                 elif i == '÷':
                     final_result += '/'
                 else:
                     final_result += i
         return final_result
-
-
-def hellp_new_hand():
-    gg = gc()
-    res_hand = ''
-    hand = gg.new_hand()
-    hand_ints = hand.integers
-    for i in hand_ints:
-        res_hand += str(i) + ' '
-    return res_hand
 
 
 def hellp_new_hand_cards(cards):
@@ -73,4 +54,61 @@ def hellp_new_hand_cards(cards):
     return cards
 
 def hellp_res_print():
-    return 'Good Job!\n\n--------------------------------------------------\n1. Try other solutions (t)\n2. Next hand (n)\n3. Show me the answers (s)\n4. Quit the game (q)\n--------------------------------------------------\n\n--------------------------------------------------\n1. Definitely no solutions (n)\n2. Give me a hint (h)\n3. I gave up, show me the answer (s)\n4. Back to the main menu (b)\n5. Quit the game (q)\n--------------------------------------------------\n\nGood Job!'
+    return '''Good Job!
+\n--------------------------------------------------
+1. Try other solutions (t)
+2. Next hand (n)\n3. Show me the answers (s)
+4. Quit the game (q)
+--------------------------------------------------
+\n--------------------------------------------------
+1. Definitely no solutions (n)
+2. Give me a hint (h)
+3. I gave up, show me the answer (s)
+4. Back to the main menu (b)
+5. Quit the game (q)
+--------------------------------------------------
+\nGood Job!'''
+
+
+class MockGame_1(gc, Hand):
+    """Creating an analogue of the 'GameConsole'
+    class with modified functions 'new_hand'"""
+    def new_hand(self):
+        """"modified function 'new_hand'"""
+        if self.is_set_end():
+            return None
+
+        cards = []
+        for i in range(self.count):
+            print(i)
+            idx = -1
+            cards.append(self.cards.pop(idx))
+        hand = Hand(cards, target=self.target)
+        self.hands.append(hand)
+        return hand
+
+
+class MockGame_5(gc, Hand):
+    """Creating an analogue of the 'GameConsole'
+    class with modified functions 'new_hand'"""
+    def new_hand(self):
+        """"modified function 'new_hand'"""
+        if self.is_set_end():
+            return None
+
+        cards = []
+        for i in range(self.count):
+            print(i)
+            idx = -5
+            cards.append(self.cards.pop(idx))
+        hand = Hand(cards, target=self.target)
+        self.hands.append(hand)
+        return hand
+
+
+def help_letters_and_numbers(a):
+    res = ''
+    for i in a:
+        if i not in '123456789':
+            return i
+    return a
